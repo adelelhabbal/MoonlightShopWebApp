4484b79587ff3d745c7adbbe4d1e81233bc6e6d3
Adel El Habbal <adelelhabbal8@gmail.com>
2021-03-25 10:49:38 +0300

    added products.html
    fixed nav navBar
    bootstrap issue discovered

--- Markup/js/global.js
+++ Markup/js/global.js
@@ -26,8 +26,21 @@ function swiper(){
         },
       });
 }
-
+function listGridView(){
+    $('#products .item').removeClass('grid-group-item');
+    $('#products .item').addClass('list-group-item');
+  $('#list').click(function(event){
+    event.preventDefault();
+    $('#products .item').addClass('list-group-item');
+  });
+  $('#grid').click(function(event){
+    event.preventDefault();
+    $('#products .item').removeClass('list-group-item');
+    $('#products .item').addClass('grid-group-item');
+  });
+}
 $(document).ready(function(){
     initDropdowns();
     swiper();
+    listGridView();
 })
